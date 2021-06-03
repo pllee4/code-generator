3f0be645d8df5694f99e36b34a36addec5c96173
pllee4 <pinloon_0428@hotmail.com>
2021-06-03 21:54:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# C code generator
+
+C code generator using [cfile](https://github.com/cogu/cfile)
+
+## To install dependencies
+```
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt-get install python3-venv
+```
+## To run in virtual environment
+```
+sudo python3 -m venv env
+source env/bin/activate
+python3 -m pip install -r requirements.txt
+python3 src/cfile_example.py
+```
+
+The generated code would be under the folder named "generated_code"
---
+++ src/cfile_example.py
@@ -0,0 +1,18 @@
+import os.path
+import sys
+import cfile as C
+
+FILE_NAME = 'hello.c'
+FILE_PATH_NAME = os.path.dirname(__file__) + '/../generated_code/' + FILE_NAME
+
+hello = C.cfile(FILE_NAME)
+hello.code.append(C.sysinclude('stdio.h'))
+hello.code.append(C.blank())
+hello.code.append(C.function('main', 'int',).add_param(C.variable('argc', 'int')).add_param(C.variable('argv', 'char', pointer=2)))
+body = C.block(innerIndent=3)
+body.append(C.statement(C.fcall('printf').add_arg(r'"Hello World!\n"')))
+body.append(C.statement('return 0'))
+hello.code.append(body)
+sys.stdout = open(FILE_PATH_NAME, "w")
+print(str(hello))
+sys.stdout.close()
